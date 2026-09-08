a9f7b3cf13c4d7d1cf7bf6dfb0a57e6eca42dd0e
matheusariel97 <psi.matheusariel@gmail.com>
2026-09-08 13:21:35 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
-import matheusPhoto from "@assets/matheus-ariel.jpg.asset.json";
-import matheusSobrePhoto from "@assets/matheus-ariel-sobre.jpg.asset.json";
+import matheusPhoto from "@assets/matheus-ariel.jpg";
+import matheusSobrePhoto from "@assets/matheus-ariel-sobre.jpg.asset";
 
 export const Route = createFileRoute("/")({
   head: () => ({
